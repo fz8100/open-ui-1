999a39db0afce26eba81fa3e108869a5dcce1bdc
Faiz Akram <fz8100@gmail.com>
2021-05-29 17:31:02 +0530

    structure created and styles added

--- open-ui/src/app/app.module.ts
+++ open-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiComponent } from './app/ui/ui.component';
+import { HeaderComponent } from './app/ui/header/header.component';
+import { ContentComponent } from './app/ui/content/content.component';
+import { CalculatorComponent } from './app/ui/content/calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent,
+    HeaderComponent,
+    ContentComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
